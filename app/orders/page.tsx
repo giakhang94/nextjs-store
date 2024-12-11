9242443e7056ded135de475211836beb460c3d60
giakhang94 <ngk.khang94@gmail.com>
2024-12-11 22:15:52 +0700

    order page

--- app/orders/page.tsx
+++ app/orders/page.tsx
@@ -1,5 +1,50 @@
-function OrdersPage() {
-  return <div>OrdersPage</div>;
+import SectionTitle from "@/components/global/SectionTitle";
+import {
+  Table,
+  TableBody,
+  TableCaption,
+  TableCell,
+  TableHead,
+  TableHeader,
+  TableRow,
+} from "@/components/ui/table";
+import { fetchUserOrders } from "@/utils/actions";
+import { formatCurrency, formatDate } from "@/utils/format";
+
+async function OrdersPage() {
+  const orders = await fetchUserOrders();
+  return (
+    <div>
+      <SectionTitle text="Your Orders" />
+      <Table>
+        <TableCaption>Total Orders: {orders.length}</TableCaption>
+        <TableHeader>
+          <TableRow>
+            <TableHead>Products</TableHead>
+            <TableHead>Order Total</TableHead>
+            <TableHead>Tax</TableHead>
+            <TableHead>Shipping</TableHead>
+            <TableHead>Date</TableHead>
+          </TableRow>
+        </TableHeader>
+        <TableBody>
+          {orders.map((order) => {
+            const { id, products, tax, shipping, orderTotal, createdAt } =
+              order;
+            return (
+              <TableRow key={id}>
+                <TableCell>{products}</TableCell>
+                <TableCell>{formatCurrency(orderTotal)}</TableCell>
+                <TableCell>{tax}</TableCell>
+                <TableCell>{shipping}</TableCell>
+                <TableCell>{formatDate(createdAt)}</TableCell>
+              </TableRow>
+            );
+          })}
+        </TableBody>
+      </Table>
+    </div>
+  );
 }
 
 export default OrdersPage;
--- utils/actions.ts
+++ utils/actions.ts
@@ -562,5 +562,53 @@ export const updateCartItemAction = async ({
 
 //order
 export const createOrderAction = async (preState: any, formData: FormData) => {
-  return { message: "created order" };
+  const user = await getAuthUser();
+  try {
+    const cart = await fetchOrCreateCart({
+      userId: user.id,
+      errorOnFailure: true,
+    });
+    await db.order.create({
+      data: {
+        clerkId: user.id,
+        products: cart.numItemsInCart,
+        orderTotal: cart.orderTotal,
+        tax: cart.tax,
+        shipping: cart.shipping,
+        email: user.emailAddresses[0].emailAddress,
+      },
+    });
+    await db.cart.delete({ where: { id: cart.id } });
+    // return { message: "Placed an order successfully" };
+  } catch (error) {
+    return renderError(error);
+  }
+  redirect("/orders");
+};
+
+export const fetchUserOrders = async () => {
+  const user = await getAuthUser();
+  const orders = await db.order.findMany({
+    where: {
+      clerkId: user.id,
+      isPaid: true,
+    },
+    orderBy: {
+      createdAt: "desc",
+    },
+  });
+  return orders;
+};
+
+export const fetchAdminOrders = async () => {
+  await getAdminUser();
+  const orders = await db.order.findMany({
+    where: {
+      isPaid: true,
+    },
+    orderBy: {
+      createdAt: "desc",
+    },
+  });
+  return orders;
 };
